c32dd70dcc497a30d818bb9f50817e2d5381e58e
Levi1709 <levi.kloeckner@fh-potsdam.de>
2025-09-30 14:48:56 +0200

    .

--- code/soundvisualisation/sketch.js
+++ code/soundvisualisation/sketch.js
@@ -235,5 +235,3 @@ class TerrainMap {
   }
 }
 
-
-// aktualisierung
--- code/task_03/sketch.js
+++ code/task_03/sketch.js
@@ -1,29 +1,28 @@
 function setup() {
   createCanvas(400, 400);
-  noFill();
   stroke(0);
 }
 
 function draw() {
   background(255);
 
-  let cols = int(map(mouseX, 0, width, 3, 12));  // grid density from mouseX
-  let rows = cols; // keep it square
+  let cols = 10;
+  let rows = 10;
   let cellSize = width / cols;
 
-  let shapeSize = map(mouseY, 0, height, 5, cellSize * 0.9); // scale with mouseY
+  // map mouse to angle
+  let angle = map(mouseX, 0, width, -PI / 4, PI / 4);
 
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       let x = i * cellSize + cellSize / 2;
       let y = j * cellSize + cellSize / 2;
 
-      if ((i + j) % 2 == 0) {
-        rectMode(CENTER);
-        rect(x, y, shapeSize, shapeSize);
-      } else {
-        ellipse(x, y, shapeSize, shapeSize);
-      }
+      push();
+      translate(x, y);
+      rotate(angle + (j * 0.1)); // slight variation by row
+      line(-cellSize / 2, 0, cellSize / 2, 0);
+      pop();
     }
   }
 }
